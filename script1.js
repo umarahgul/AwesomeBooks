3c83fd5aaaed2156ee0baa834a7d35f4ee908b55
umarahgul <umarah.gul@gmail.com>
2023-08-09 03:52:53 +0500

    removed linter errors

--- script1.js
+++ script1.js
@@ -1,100 +1,73 @@
+// an array to hold object book
 const collection = [
-{title:"title1", author:"author1"},
-{title:"title2", author:"author2"},
-{title:"title2", author:"author2"},
-{title:"title2", author:"author2"}
-]
+  { title: 'title1', author: 'auhtor1' },
+  { title: 'title2', author: 'auhtor2' },
+  { title: 'title3', author: 'auhtor3' },
+  { title: 'title4', author: 'auhtor4' },
+];
 
-function addBooks(x)
-{
-        const displayDiv =document.createElement('div');   
-         const para1=document.createElement('p');
-         console.log("value added in the text field = " + document.getElementById('title').textContent);
-        
-         para1.textContent=document.getElementById('title').textContent;
-         
-         displayDiv.appendChild(para1);
-        
-        const para2=document.createElement('p');
-        para2.textContent=document.getElementById('author').textContent;
-        displayDiv.appendChild(para2);
-    
-    
-         const removeButton =document.createElement('button');     
-         removeButton.textContent="Remove";
-         displayDiv.appendChild(removeButton);
-    
-        (function (index) {
-        removeButton.onclick =function () {
-        console.log("removed " + collection[x].title);
-         removeBook(x); 
-         displayDiv.removeChild(para1);
-         displayDiv.removeChild(para2);
-         displayDiv.removeChild(removeButton); 
-         displayDiv.remove();         
-    }
-    
-     })(x);
-    
-    const divider=document.createElement('hr');
-    displayDiv.appendChild(divider);
-    main.appendChild(displayDiv);
-
-}
-
-// display books  if available in collection
+const titleVal = document.getElementById('title');
+const authorVal = document.getElementById('author');
 const main = document.getElementById('awesome');
 
-function displayBooks()
-{
-    
-   
-    if(collection.length>0)
-    {
+function displayBooks() {
+  main.innerHTML = '';
+  if (collection.length > 0) {
+    for (let x = 0; x < collection.length; x += 1) {
+      const displayDiv = document.createElement('div');
+      const para1 = document.createElement('p');
+      const para2 = document.createElement('p');
+      const removeButton = document.createElement('button');
+      removeButton.textContent = 'Remove';
 
-    for(let x=0; x<collection.length; x++)
-    {    
-        const displayDiv =document.createElement('div');   
-         const para1=document.createElement('p');
-         para1.textContent=collection[x].title;
-         
-         displayDiv.appendChild(para1);
-        
-        const para2=document.createElement('p');
-        para2.textContent=collection[x].author;
-        displayDiv.appendChild(para2);
-    
-    
-         const removeButton =document.createElement('button');     
-         removeButton.textContent="Remove";
-         displayDiv.appendChild(removeButton);
-    
-        (function (index) {
-        removeButton.onclick =function () {
-        console.log("removed " + collection[x].title);
-         removeBook(x); 
-         displayDiv.removeChild(para1);
-         displayDiv.removeChild(para2);
-         displayDiv.removeChild(removeButton); 
-         displayDiv.remove();         
+      para1.textContent = collection[x].title;
+      displayDiv.setAttribute('id', `id${x}`);
+      console.log(displayDiv.id);
+      displayDiv.appendChild(para1);
+      // console.log(para1.textContent);
+      para2.textContent = collection[x].author;
+      displayDiv.appendChild(para2);
+      removeButton.textContent = 'Remove';
+      displayDiv.appendChild(removeButton);
+
+      const divider = document.createElement('hr');
+      displayDiv.appendChild(divider);
+      main.appendChild(displayDiv);
+      removeButton.addEventListener('click', () => {
+        displayDiv.remove();
+        collection.splice(x, 1);
+      });
     }
-    
-     })(x);
-    
-    const divider=document.createElement('hr');
-    displayDiv.appendChild(divider);
-    main.appendChild(displayDiv);
-
-        
-} // for loop closes
-}  // if statement closes
-
-else {
-
-    alert("Empty Collection");
+  } else {
+    alert('Empty Collection');
+  }
 }
-    
-} // function closes
 
-document.addEventListener('DOMContentLoaded',displayBooks);
+function addBooks() {
+  collection.push({ title: `${titleVal.value}`, author: `${authorVal.value}` });
+  for (let i = 0; i < collection.length; i += 1) { console.log(collection[i]); }
+  displayBooks();
+}
+document.getElementById('addBook').addEventListener('click', addBooks);
+document.addEventListener('DOMContentLoaded', displayBooks);
 
+// if(!localStorage.getItem("title")){
+//     setStorage();
+// }
+// else
+// {
+//     getStorage();
+// }
+
+// function getStorage() {
+//     const currentTitle = localStorage.getItem('title');
+//     const currentAuthor = localStorage.getItem('author');
+
+//     titleVal.value = currentTitle;
+//     document.getElementById('author').value = currentAuthor;
+//   }
+
+//   function setStorage() {
+//     localStorage.setItem('title', document.getElementById('title').value);
+//     localStorage.setItem('author', document.getElementById('author').value);
+//   }
